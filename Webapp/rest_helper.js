37b130a548dc476537e162ad8030dac84305525f
Y.Arun Ramanathan <y.arunram@gmail.com>
2018-08-19 06:30:11 -0700

    Additional routes, rest client integration

---
+++ Webapp/rest_helper.js
@@ -0,0 +1,76 @@
+var Client = require('node-rest-client').Client;
+var client = new Client();
+var successStatusCode = 200;
+var request = require("request");
+
+var defaultPostArgs = {
+          headers: { "Content-Type": "application/json" }
+  };
+
+module.exports = {
+
+	sendRestPostRequest : function(url,args,callback,req,res){
+		console.log(url);
+		console.log(JSON.stringify(args));
+		// client.post(url,args, function (data, response) {
+		//     callback(data,response.statusCode,req,res);
+		// });
+		
+		var options = { method: 'POST',
+		  url: url,
+		  headers: 
+		   {'cache-control': 'no-cache' },
+		  body: JSON.stringify(args) };
+
+		request(options, function (error, response, body) {
+		  if (error) throw new Error(error);
+
+		  callback(body,response.statusCode,req,res);
+		});
+
+	},
+
+	sendRestGetRequest : function(url,callback,req,res){
+
+		var options = { method: 'GET',
+						  url: url,
+						  headers: 
+						   {'content-type': 'application/json' } };
+
+
+		request(options, function (error, response, body) {
+		  if (error) throw new Error(error);
+		  callback(body,response.statusCode,req,res);
+		  console.log(body);
+		});
+
+	}
+}
+
+loginPostCallback = function(data,statusCode,req,res){
+    console.log(data);
+    //console.log(JSON.parse(data).id);
+    console.log(statusCode);
+
+    if(statusCode == successStatusCode){
+      req.session.isLoggedIn = true;
+      req.session.userid = JSON.parse(data).id;
+      console.log("user" + req.session.userid);
+    }
+    else{
+     req.session.isLoggedIn = false;
+    }
+    res.send(statusCode.toString()); 
+};
+
+getCallback = function(data,statusCode,req,res){
+	console.log(data);
+    console.log(statusCode);
+    res.send(JSON.stringify(data));
+}
+
+postComplaintCallback = function(data,statusCode,req,res){
+    console.log(data);
+    console.log(statusCode);
+    res.send(statusCode.toString()); 
+};
--- Webapp/routes.js
+++ Webapp/routes.js
@@ -1,5 +1,32 @@
 var utils = require('./utils');
 var session = require('express-session');
+var rest_api_url = "https://erqnzqlowa.execute-api.us-west-1.amazonaws.com/Prod";
+var rest_helper = require('./rest_helper');
+//login url and args
+var login_url = rest_api_url + "/login";
+var login_args = {
+	"email" : "somedude@foobar.com",
+	"password" : "password"
+}
+
+//parks url
+var get_parks_url = rest_api_url + "/parks";
+
+
+//categories url
+var get_categories_url = rest_api_url + "/categories";
+
+var complaints_url = rest_api_url + "/complaints";
+
+var complaint_args = {
+      "park": "a2ad0f1d-1c45-4423-9fbe-cd43b3cf2759",
+      "user": "0178c1df-b995-4320-b428-ee702bc20d4f",
+      "image_url": "url to S3 bucket",
+      "description": "This is a sample description",
+      "title": "Title Sample",
+      "category": "f4d7f5e3-4373-4570-ba0c-ba2c82908609",
+      "callback" : "Yes"
+    }
 
 module.exports = function(app){
 	
@@ -17,16 +44,43 @@ module.exports = function(app){
 
 	/* GET home page. */
 	app.get('/home', function(req, res) {
-	  req.session.isLoggedIn = true;
 	  utils.redirectToPage("home.html",req,res);
 	});
 
+	app.get('/registercomplaint', function(req, res) {
+	  utils.redirectToPage("complaint.html",req,res);
+	});
+
+	app.get('/park/:id', function(req, res) {
+	  utils.redirectToPage("park.html",req,res);
+	});
+
 	app.get('/processlogin', function(req, res) {
-	  var password  = req.query.password;
-	  var email = req.query.email;
-	  console.log(loginGetUrl+"?email="+email+"&password="+password);
-	  sendRestGetRequest(loginGetUrl+"?email="+email+"&password="+password,loginPostCallback,req,res);
-	  //loginPostCallback("data",successStatusCode,req,res);
+	   var args = login_args;
+	   args.email = req.query.email;
+	   args.password = req.query.password;
+	   
+	   rest_helper.sendRestPostRequest(login_url,args,loginPostCallback,req,res);
+	});
+
+	app.get('/getparks', function(req, res) {
+	  rest_helper.sendRestGetRequest(get_parks_url,getCallback,req,res);
+	});
+
+	app.get('/getcategories', function(req, res) {
+	  rest_helper.sendRestGetRequest(get_categories_url,getCallback,req,res);
+	});
+
+	app.get('/postcomplaint', function(req, res) {
+	  complaint_args.park =  req.query.park;
+      complaint_args.user = req.session.userid;
+      complaint_args.image_url = "url to S3 bucket";
+      complaint_args.description = req.query.description;
+      complaint_args.title = req.query.title;
+      complaint_args.category = req.query.category;
+      complaint_args.callback = req.query.callback;
+      console.log("user" + req.session.userid);
+	  rest_helper.sendRestPostRequest(complaints_url,complaint_args,postComplaintCallback,req,res);
 	});
 
     //other routes..
